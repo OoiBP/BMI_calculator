e91256ca3c2d9e9738f1687eba7bde55160a49d3
OoiBP <bpooi0702@gmail.com>
2020-02-15 22:39:28 +0800

    Add: Useless Drawer Gender Button Weight Slider Height Slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,276 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final Widget viewDrawer = Drawer(
+    child: ListView(
+      padding: EdgeInsets.zero,
+      children: const <Widget>[
+        DrawerHeader(
+          decoration: BoxDecoration(
+            color: Colors.blueAccent,
+          ),
+          child: Text(
+            'Customize It',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.message),
+          title: Text('Messages'),
+        ),
+        ListTile(
+          leading: Icon(Icons.account_circle),
+          title: Text('Profile'),
+        ),
+        ListTile(
+          leading: Icon(Icons.settings),
+          title: Text('Settings'),
+        ),
+      ],
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: "BMI Calculator",
+      theme: ThemeData(primaryColor: Colors.blueAccent),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("BMI Calculator"),
+        ),
+        drawer: viewDrawer,
+        body: MainView(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MainView extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  MainViewState createState() => MainViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MainViewState extends State<MainView> {
+  bool _isMale = true;
+  num _weight = 50.0;
+  num _height = 170.0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    void _changeMale() {
+      setState(() {
+        _isMale = true;
+      });
+    }
+
+    void _changeFemale() {
+      setState(() {
+        _isMale = false;
+      });
+    }
+
+    void _weightSliderChange(double newValue) {
+      setState(() {
+        _weight = newValue.toInt();
+      });
+    }
+
+    void _heightSliderChange(double newValue) {
+      setState(() {
+        _height = newValue.toInt();
+      });
+    }
+
+    Widget txtLabel(String text, num item) {
+      return Container(
+        margin: EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 5.0),
+        padding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+        child: Text(
+          "$text: $item",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        alignment: Alignment.centerLeft,
+      );
+    }
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        children: <Widget>[
+          GenderSelectionView(_changeMale, _changeFemale),
+          txtLabel("Weight", _weight),
+          WeightSlider(_weight, _weightSliderChange),
+          txtLabel("Height", _height),
+          HeightSlider(_height, _heightSliderChange),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    );
+  }
+}
+
+class GenderSelectionView extends StatelessWidget {
+  final _onPressMale;
+  final _onPressFemale;
+
+  GenderSelectionView(this._onPressMale, this._onPressFemale);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        MaleGenderButton(_onPressMale),
+        FemaleGenderButton(_onPressFemale),
+      ],
+    );
+  }
+}
+
+class MaleGenderButton extends StatelessWidget {
+  final Function _onPress;
+
+  MaleGenderButton(this._onPress);
+
+  static const boy = LinearGradient(
+    colors: <Color>[
+      Colors.blue,
+      Color(0xFF64B5F6),
+      Color(0xFFBBDEFB),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(0.0),
+      decoration: new BoxDecoration(
+        borderRadius: new BorderRadius.circular(10.0),
+        gradient: boy,
+      ),
+      child: FlatButton.icon(
+        padding: EdgeInsets.all(30.0),
+        color: Colors.transparent,
+        icon: Icon(MdiIcons.genderMale),
+        onPressed: _onPress,
+        label: Text("Male"),
+      ),
+    );
+  }
+}
+
+class FemaleGenderButton extends StatelessWidget {
+  final Function _onPress;
+
+  FemaleGenderButton(this._onPress);
+
+  static const girl = LinearGradient(
+    colors: <Color>[
+      Colors.pink,
+      Color(0xFFF06292),
+      Color(0xFFF8BBD0),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(0.0),
+      decoration: new BoxDecoration(
+        borderRadius: new BorderRadius.circular(10.0),
+        gradient: girl,
+      ),
+      child: FlatButton.icon(
+        padding: EdgeInsets.all(30.0),
+        color: Colors.transparent,
+        icon: Icon(MdiIcons.genderFemale),
+        onPressed: _onPress,
+        label: Text("Female"),
+      ),
+    );
+  }
+}
+
+class WeightSlider extends StatelessWidget {
+  final num _value;
+  final Function _onChange;
+
+  WeightSlider(this._value, this._onChange);
+
+  @override
+  Widget build(BuildContext context) {
+    final double _minimum = 40.0;
+    final double _maximum = 200.0;
+    final int _divs = (_maximum - _minimum + 1.0).toInt();
+
+    return Slider(
+      value: _value.toDouble(),
+      onChanged: _onChange,
+      min: _minimum,
+      max: _maximum,
+      label: _value.toString(),
+      divisions: _divs,
+    );
+  }
+}
+
+class HeightSlider extends StatelessWidget {
+  final num _value;
+  final Function _onChange;
+
+  HeightSlider(this._value, this._onChange);
+
+  @override
+  Widget build(BuildContext context) {
+    final double _minimum = 120.0;
+    final double _maximum = 230.0;
+    final int _divs = (_maximum - _minimum + 1.0).toInt();
+
+    return Slider(
+      value: _value.toDouble(),
+      onChanged: _onChange,
+      min: _minimum,
+      max: _maximum,
+      label: _value.toString(),
+      divisions: _divs,
+    );
+  }
+}
+
+class CardView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {/* ... */},
+                ),
+                FlatButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {/* ... */},
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
